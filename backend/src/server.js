6e684825d95c0c2bb2e36b4455b6df551dd99361
michaeljohnarevalo <michaelarevaloooo@gmail.com>
2026-02-17 12:57:10 +0800

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import  dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
 
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 //console.log(process.env.MONGO_URI)
@@ -12,15 +13,19 @@ dotenv.config()
 
 const app = express()
 const PORT =process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //connectDB();
 
 //middleware
-app.use(cors(
-    {
-        origin:"http://localhost:5173"
-    }
-))
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors(
+        {
+            origin:"http://localhost:5173"
+        }
+    ))
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -33,6 +38,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes)
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"))
+    });
+}
 
 //http://localhost:5001/api/notes/21
 
